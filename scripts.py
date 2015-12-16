f640393abfb4269172f534daff6c069c6b25ddb9
Amy Slagle <amyslagle@nypl.org>
2015-12-16 11:59:00 -0500

    Added script for importing Plympton books.

--- scripts.py
+++ scripts.py
@@ -1,12 +1,38 @@
 import os
+import datetime
+datetime.datetime.strptime("2015","%Y")
+from pymarc import MARCReader
 from core.scripts import Script
 from monitor import GutenbergMonitor
 from coverage import (
     GutenbergIllustratedCoverageProvider,
     GutenbergEPUBCoverageProvider,
 )
-from core.model import LicensePool
+from core.model import (
+    LicensePool,
+    DataSource,
+    Edition,
+    Identifier,
+    Representation,
+    DeliveryMechanism,
+    Contributor,
+    Hyperlink,
+    get_one,
+)
+from core.classifier import Classifier
 from core.monitor import PresentationReadyMonitor
+
+from core.metadata_layer import (
+    ContributorData,
+    Metadata,
+    LinkData,
+    IdentifierData,
+    FormatData,
+    MeasurementData,
+    SubjectData,
+)
+from core.s3 import S3Uploader
+
 
 class GutenbergMonitorScript(Script):
 
@@ -105,3 +131,104 @@ class MakePresentationReadyScript(Script):
         providers = [illustrated, epub]
         PresentationReadyMonitor(
             self._db, providers, calculate_work_even_if_no_author=True).run()
+
+class DirectoryImportScript(Script):
+
+    def run(self, directory, data_source_name):
+        # Look for a MARC metadata file in the directory
+        metadata_file = None
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith(".mrc") or file.endswith(".marc"):
+                    metadata_file = os.path.join(root, file)
+                    break
+            if metadata_file:
+                break
+
+        if not metadata_file:
+            raise Exception("No metadata file found")
+
+        # Extract metadata for each book
+        with open(metadata_file) as f:
+            reader = MARCReader(f)
+            for record in reader:
+                title = record.title()
+                issued_year = datetime.datetime.strptime(record.pubyear(), "%Y.")
+                publisher = record.publisher()
+                summary = record.notes()[0]['a']
+
+                isbn = record['020']['a'].split(" ")[0]
+                primary_identifier = IdentifierData(
+                    Identifier.ISBN, isbn
+                )
+
+                subjects = [SubjectData(
+                    Classifier.LCSH,
+                    subject['a'],
+                ) for subject in record.subjects()]
+
+                author = record.author()
+                contributors = [ContributorData(
+                    sort_name=author,
+                    roles=[Contributor.AUTHOR_ROLE],
+                )]
+                
+                uploader = S3Uploader()
+                links = []
+
+                paths = dict()
+
+                url = uploader.book_url(primary_identifier, "epub")
+                epub_path = os.path.join(directory, isbn + ".epub")
+                paths[url] = epub_path
+
+                epub_link = LinkData(
+                    rel=Hyperlink.OPEN_ACCESS_DOWNLOAD,
+                    href=url,
+                    media_type=Representation.EPUB_MEDIA_TYPE,
+                )
+
+                formats = [FormatData(
+                    content_type=Representation.EPUB_MEDIA_TYPE,
+                    drm_scheme=DeliveryMechanism.NO_DRM,
+                    link=epub_link,
+                )]
+                links.append(epub_link)
+
+                cover_file = isbn + ".jpg"
+                cover_url = uploader.cover_image_url(
+                    data_source_name, primary_identifier, cover_file)
+                cover_path = os.path.join(directory, cover_file)
+                paths[cover_url] = cover_path
+
+                links.append(LinkData(
+                    rel=Hyperlink.IMAGE,
+                    href=cover_url,
+                    media_type=Representation.JPEG_MEDIA_TYPE,
+                ))
+
+            
+                metadata = Metadata(
+                    data_source=data_source_name,
+                    book_data_source=data_source_name,
+                    title=title,
+                    language='eng',
+                    medium=Edition.BOOK_MEDIUM,
+                    publisher=publisher,
+                    issued=issued_year,
+                    primary_identifier=primary_identifier,
+                    subjects=subjects,
+                    contributors=contributors,
+                    formats=formats,
+                    links=links,
+                )
+                license_pool, new = metadata.license_pool(self._db)
+                edition, new = metadata.edition(self._db)
+                if new:
+                    print "created new edition %s" % edition.title
+                
+                for link in edition.primary_identifier.links:
+                    representation = link.resource.representation
+                    representation.mirror_url = link.resource.url
+                    representation.local_content_path = paths[link.resource.url]
+                    uploader.mirror_one(representation)                    
