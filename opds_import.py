0dc1c9c0872b68bae8cfdd8cd35b82c5f05961b0
Leonard Richardson <leonardr@segfault.org>
2016-03-25 10:25:53 -0400

    Pass an S3Uploader into the OPDSImporter to automatically mirror scaled images to S3.

--- opds_import.py
+++ opds_import.py
@@ -5,50 +5,16 @@ from core.model import (
     Hyperlink,
     DataSource,
 )
-from urllib import urlopen
 from copy import deepcopy
 from nose.tools import set_trace
 
 class ContentOPDSImporter(OPDSImporter):
     """OPDS Importer that mirrors content to S3."""
 
-    def import_from_feed(self, feed, even_if_no_author=False):
-        imported, messages, next_links = super(ContentOPDSImporter, self).import_from_feed(feed, even_if_no_author=even_if_no_author)
-
-        # Mirror books and images to S3 for the imported editions
-        uploader = S3Uploader()
-        for edition in imported:
-            links = edition.primary_identifier.links
-            for link in links:
-                representation = link.resource.representation
-                media_type = representation.media_type
-                original_url = link.resource.url
-
-                mirror_url = None
-                if media_type in Representation.BOOK_MEDIA_TYPES:
-                    extension = Representation.FILE_EXTENSIONS.get(media_type)
-                    mirror_url = uploader.book_url(edition.primary_identifier, extension=extension, title=edition.title)
-                elif link.rel == Hyperlink.THUMBNAIL_IMAGE or link.rel == Hyperlink.IMAGE:
-                    data_source = DataSource.lookup(self._db, self.data_source_name)
-                    mirror_url = uploader.cover_image_url(data_source, edition.primary_identifier, original_url.split("/")[-1])
-               
-                if mirror_url:
-                    try:
-                        representation.mirror_url = mirror_url
-                        handle = urlopen(original_url)
-                        content = handle.read()
-                        handle.close()
-                        representation.content = content
-                        uploader.mirror_one(representation)
-                        representation.content = None
-                        edition.work.set_presentation_ready()
-                    except (IOError, TypeError) as e:
-                        print "Unable to mirror %s" % original_url
-                        representation.mirror_exception = str(e)
-                        representation.mirror_url = None
-
-        return imported, messages, next_links
-
+    def __init__(self, *args, **kwargs):
+        super(ContentOPDSImporter, self).__init__(
+            *args, mirror=S3Uploader(), **kwargs
+        )
 
 class UnglueItOPDSImporter(ContentOPDSImporter):
     """Importer for unglue.it OPDS feed, which has acquisition links from multiple sources for some entries."""
