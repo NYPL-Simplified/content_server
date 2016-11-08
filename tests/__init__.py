1995051961a7b67eecb7053aa13fee76a697871f
Courteney Ervin <courteneyervin@nypl.org>
2016-11-08 10:59:43 -0500

    Refactor sample_data method in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+import sys, os
 from nose.tools import set_trace
 
 from ..core.testing import (
@@ -6,3 +7,9 @@ from ..core.testing import (
 )
 
 package_setup()
+
+def sample_data(filename, sample_data_dir):
+    base_path = os.path.split(__file__)[0]
+    resource_path = os.path.join(base_path, "files", sample_data_dir)
+    path = os.path.join(resource_path, filename)
+    return open(path).read()
