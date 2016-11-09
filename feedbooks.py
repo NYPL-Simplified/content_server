f58d96ab06bf1ea6c319c278fa6678edbfcdb2bf
Leonard Richardson <leonardr@segfault.org>
2016-11-09 09:54:42 -0500

    Added and tested RehostingPolicy.

--- feedbooks.py
+++ feedbooks.py
@@ -7,6 +7,7 @@ from core.model import (
     Hyperlink,
     Resource,
     Representation,
+    RightsStatus,
 )
 
 class FeedbooksOPDSImporter(OPDSImporter):
@@ -20,7 +21,24 @@ class FeedbooksOPDSImporter(OPDSImporter):
         for id, metadata in metadata.items():
             self.improve_description(id, metadata)
         return metadata, failures
-        
+
+    def rights_for_entry(self, entry):
+        """Determine the URI that best encapsulates the rights status of
+        the downloads associated with this book.
+
+        Most of the time this will be CC-BY-NC, the rights status
+        associated with Feedbooks, or "full copyright", for books that
+        are redistributable in France but not in the US. In rare cases
+        it will be some other CC license that prohibits Feedbooks from
+        relicensing as CC-BY-NC.
+
+        :return: A URI.
+        """
+        rights = entry.get('rights', "")
+        source = entry.get('dcterms_source', '')
+        publication_year = entry.get('dcterms_issued', None)
+        return RehostingPolicy.rights_uri(rights, source, publication_year)
+            
     def improve_description(self, id, metadata):
         """Improve the description associated with a book,
         if possible.
@@ -81,3 +99,127 @@ class FeedbooksOPDSImporter(OPDSImporter):
                     everything_except_descriptions + new_descriptions
                 )
         return metadata
+
+
+class RehostingPolicy(object):
+    """Determining the precise copyright status of the underlying text
+    is not directly useful, because Feedbooks has made derivative
+    works and relicensed under CC-BY-NC. So that's going to be the
+    license: CC-BY-NC.
+    
+    Except it's not that simple. There are two complications.
+    
+    1. Feedbooks is located in France, and NYPL's open-access
+    content server is hosted in the US. We can't host a CC-BY-NC
+    book if it's derived from a work that's still under US
+    copyright. We must decide whether or not to accept a book in the
+    first place based on the copyright status of the underlying
+    text.
+    
+    2. Some CC licenses are more restrictive (on the creators of
+    derivative works) than CC-BY-NC. Feedbooks has no authority to
+    relicense these books, so they need to be preserved.
+
+    This class encapsulates the logic necessary to make this decision.
+    """
+    
+    PUBLIC_DOMAIN_CUTOFF = 1923    
+
+    # These are the licenses that need to be preserved.
+    RIGHTS_DICT = {    
+        "Attribution Share Alike (cc by-sa)" : RightsStatus.CC_BY_SA,
+        "Attribution Non-Commercial No Derivatives (cc by-nc-nd)" : RightsStatus.CC_BY_NC_ND,
+        "Attribution Non-Commercial Share Alike (cc by-nc-sa)" : RightsStatus.CC_BY_NC_SA,
+    }
+    
+    # Feedbooks rights statuses indicating books that can be rehosted
+    # in the US.
+    CAN_REHOST_IN_US = set([
+        "This work was published before 1923 and is in the public domain in the USA only.",
+        "This work is available for countries where copyright is Life+70 and in the USA.",
+        'This work is available for countries where copyright is Life+50 or in the USA (published before 1923).',
+        "Attribution (cc by)",
+        "Attribution Non-Commercial (cc by-nc)",
+
+        "Attribution Share Alike (cc by-sa)",
+        "Attribution Non-Commercial No Derivatives (cc by-nc-nd)",
+        "Attribution Non-Commercial Share Alike (cc by-nc-sa)",
+    ])
+
+    # These websites are hosted in the US and specialize in
+    # open-access content. We will accept all FeedBooks titles taken
+    # from these sites, even post-1923 titles.
+    US_SITES = set([
+        "archive.org",
+        "craphound.com",
+        "en.wikipedia.org",
+        "en.wikisource.org",
+        "futurismic.com",
+        "gutenberg.org",
+        "project gutenberg",
+        "shakespeare.mit.edu",
+    ])
+
+    @classmethod
+    def rights_uri(cls, rights, source, publication_year):
+        if not cls.can_rehost_us(rights, source, publication_year):
+            # We believe this book is still under copyright in the US
+            # and we should not rehost it.
+            return RightsStatus.IN_COPYRIGHT
+
+        if rights in cls.RIGHTS_DICT:
+            # The CC license of the underlying text means it cannot be
+            # relicensed CC-BY-NC.
+            return cls.RIGHTS_DICT[rights]
+
+        # The default license as per our agreement with FeedBooks.
+        return RightsStatus.CC_BY_NC
+    
+    @classmethod
+    def can_rehost_us(cls, rights, source, publication_year):
+        """Can we rehost this book on a US server?
+
+        :param rights: What FeedBooks says about the public domain status
+        of the book.
+
+        :param source: Where FeedBooks got the book.
+
+        :param publication_year: When the text was originally published.
+
+        :return: True or False
+        """
+        if publication_year < cls.PUBLIC_DOMAIN_CUTOFF:
+            # We will rehost anything published prior to 1923, no
+            # matter where it came from.
+            return True
+        
+        if rights in cls.CAN_REHOST_IN_US:
+            # This book's FeedBooks rights statement explicitly marks
+            # it as one that can be rehosted in the US.
+            return True
+            
+        # The rights statement isn't especially helpful, but maybe we
+        # can make a determination based on where Feedbooks got the
+        # book from.
+        source = (source or "").lower()
+
+        if any(site in source for site in cls.US_SITES):
+            # This book originally came from a US-hosted site that
+            # specializes in open-access books, so we must be able
+            # to rehost it.
+            return True
+
+        if source in ('wikisource', 'gutenberg'):
+            # Presumably en.wikisource and Project Gutenberg US.  We
+            # special case these to avoid confusing the US versions of
+            # these sites with other countries'.
+            return True
+                
+        # And we special-case this one to avoid confusing Australian
+        # Project Gutenberg with US Project Gutenberg.
+        if ('gutenberg.net' in source and not 'gutenberg.net.au' in source):
+            return True
+
+        # Unless one of the above conditions is met, we must assume
+        # the book cannot be rehosted in the US.
+        return False
